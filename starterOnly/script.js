5f08dfb24466757009cbc7620ee41254beb8ff33
Stanley <stanley.defer@hotmail.Fr>
2024-04-24 17:45:20 +0200

    🚧 Add all missing inputs / still some WIP

--- starterOnly/script.js
+++ starterOnly/script.js
@@ -1,25 +1,42 @@
+// Checking if submit btn is clicked then starting the form
 let form = document.querySelector("form")
-    form.addEventListener("submit", (e) => {
-        e.preventDefault()
-        handleForm()
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    handleForm()
+}) 
+
+// Listening to the tournament radio choice
+ 
+let tournamentList = document.querySelectorAll("form input[type=radio]")
+for (let i = 0; i < tournamentList.length; i++) {
+    tournamentList[i].addEventListener("change", () => {
+        let choosenTournament = tournamentList[i].value
     })
-
-function handleForm(){
-
-        let firstname = document.getElementById("first").value
-        let lastname = document.getElementById("last").value
-        let email = document.getElementById("email").value
-        let birthdate = document.getElementById("birthdate").value
-        
-        firstnameValidation(firstname)
-        lastnameValidation(lastname)
-        emailValidation(email)
-        birthdateValidation(birthdate)
-
-        console.log(firstname, lastname, email, birthdate)
 }
 
+function handleForm(choosenTournament){
+    //Getting all the inputs
+    let firstname = document.getElementById("first").value
+    let lastname = document.getElementById("last").value
+    let email = document.getElementById("email").value
+    let birthdate = document.getElementById("birthdate").value
+    let tournamentQuantity = document.getElementById("quantity").value
+    let newsletter = document.getElementById("checkbox2").checked
 
+
+    firstnameValidation(firstname)
+    lastnameValidation(lastname)
+    emailValidation(email)
+    birthdateValidation(birthdate)
+    tournamentQuantityValidation(tournamentQuantity)
+    tournamentChoice(choosenTournament)
+    tournamentChoiceValidation(choosenTournament)
+    termOfUse()
+
+    console.log(firstname, lastname, email, birthdate, tournamentQuantity, choosenTournament, newsletter)
+
+}
 
 function firstnameValidation(firstname){
     if (firstname.length < 2) {
@@ -46,3 +63,34 @@ function birthdateValidation(birthdate){
     }
 }
 
+function tournamentQuantityValidation(tournamentQuantity){
+    
+    if (isNaN(tournamentQuantity)){
+        throw new Error ("Quantité invalide")
+    }
+}
+
+/*
+function tournamentChoice(choosenTournament){
+    let tournamentList = document.querySelectorAll("form input[type=radio]")
+    console.log(tournamentList)
+    for (let i = 0; i < tournamentList.length; i++) {
+        choosenTournament = tournamentList[i].value
+        console.log(choosenTournament)
+    }
+}*/
+
+function tournamentChoiceValidation(choosenTournament){
+    if (choosenTournament === ""){
+        console.log(choosenTournament)
+        throw new Error ("Veuillez choisir un tournoi")
+    }
+}
+
+function termOfUse(){
+    let checkbox = document.getElementById("checkbox1").checked
+
+    if (checkbox === false){
+        throw new Error ("Veuillez accepter les conditions d'utilisation")
+    }
+}
